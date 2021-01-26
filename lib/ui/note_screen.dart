d9eda0968f8e042e103535073478ecd3be036f22
Kumar Gautam <kumar.g@tataelxsi.co.in>
2021-01-26 21:06:02 +0530

    first commit

---
+++ lib/ui/note_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:Maersk/model/listfile.dart';
+import 'package:Maersk/util/database_helper.dart';
+ 
+class NoteScreen extends StatefulWidget {
+  final Note note;
+  NoteScreen(this.note);
+  
+ 
+  @override
+  State<StatefulWidget> createState() => new _NoteScreenState();
+}
+ 
+class _NoteScreenState extends State<NoteScreen> {
+  DatabaseHelper db = new DatabaseHelper();
+ 
+  TextEditingController _titleController;
+  TextEditingController _descriptionController;
+
+  String dropdownValue = 'One';
+ 
+  @override
+  void initState() {
+    super.initState();
+ 
+    _titleController = new TextEditingController(text: widget.note.title);
+    _descriptionController = new TextEditingController(text: widget.note.description);
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Book Meetings')),
+      body: Container(
+        margin: EdgeInsets.all(15.0),
+        alignment: Alignment.center,
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(labelText: 'Title'),
+            ),
+            Padding(padding: new EdgeInsets.all(5.0)),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(labelText: 'Description'),
+            ),
+            DropdownButton<String>(
+    value: dropdownValue ,
+    icon: Icon(Icons.arrow_downward),
+    iconSize: 24,
+    elevation: 16,
+    style: TextStyle(
+      color: Colors.deepPurple
+    ),
+    underline: Container(
+      height: 2,
+      color: Colors.deepPurpleAccent,
+    ),
+    onChanged: (String newValue) {
+      setState(() {
+        dropdownValue = newValue;
+      });
+    },
+    items: <String>['One', 'Two', 'Three', 'Four']
+      .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      })
+      .toList(),
+  ),
+            Padding(padding: new EdgeInsets.all(5.0)),
+            RaisedButton(
+              child: (widget.note.id != null) ? Text('Update') : Text('Add'),
+              onPressed: () {
+                if (widget.note.id != null) {
+                  db.updateNote(Note.fromMap({
+                    'id': widget.note.id,
+                    'title': _titleController.text,
+                    'description': _descriptionController.text
+                  })).then((_) {
+                    Navigator.pop(context, 'update');
+                  });
+                }else {
+                  db.saveNote(Note(_titleController.text, _descriptionController.text)).then((_) {
+                    Navigator.pop(context, 'save');
+                  });
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
